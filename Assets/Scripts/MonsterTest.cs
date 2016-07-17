d6b35d5bfb3957f50244680de1079642415c4597
Andrew Palmer <andyp.123@gmail.com>
2016-07-17 22:03:53 +0900

    Add somewhat hacky monster spawner
    Just wanted to get a basic wave implementation working.

--- Assets/Scripts/MonsterTest.cs
+++ Assets/Scripts/MonsterTest.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public abstract class Monster : MonoBehaviour
+public class MonsterTest : Monster
 {
 
 }
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave : MonoBehaviour
+{
+  public string _monsterType = ""; // monster type to spawn
+  public int _spawnCount = 10; // number of monsters in the wave
+  public float _spawnDelay = 1f; // spawn next monster after this many seconds
+  public Path _path; // which path to spawn monsters on
+
+  // quick hack for now
+  public InstancedObjectManager _objectManager;
+
+
+  void Start ()
+  {
+    if (_objectManager != null)
+     StartCoroutine(SpawnMonsters());
+  }
+
+  IEnumerator SpawnMonsters ()
+  {
+    for (int i = 0; i < _spawnCount; ++i)
+    {
+      Debug.Log("spawn monster");
+      GameObject monster = _objectManager.Instantiate(_monsterType, Vector3.zero, Quaternion.identity);
+      if (_path != null && monster != null)
+      {
+        PathMover pm = monster.GetComponent<PathMover>();
+        if (pm != null)
+          pm._path = _path;
+      }
+      yield return new WaitForSeconds(_spawnDelay);
+    }
+  }
+}
