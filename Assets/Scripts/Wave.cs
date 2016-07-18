29d9e2d84fdfce8cce4bc1c47262314a81cf1194
Andrew Palmer <andyp.123@gmail.com>
2016-07-18 11:25:11 +0900

    Add wave start delay functionality

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -5,6 +5,7 @@ public class Wave : MonoBehaviour
 {
   public string _monsterType = ""; // monster type to spawn
   public int _spawnCount = 10; // number of monsters in the wave
+  public float _startDelay = 0f; // delay before first monster is spawned
   public float _spawnDelay = 1f; // spawn next monster after this many seconds
   public Path _path; // which path to spawn monsters on
 
@@ -20,9 +21,10 @@ public class Wave : MonoBehaviour
 
   IEnumerator SpawnMonsters ()
   {
+    yield return new WaitForSeconds(_startDelay);
+
     for (int i = 0; i < _spawnCount; ++i)
     {
-      Debug.Log("spawn monster");
       GameObject monster = _objectManager.Instantiate(_monsterType, Vector3.zero, Quaternion.identity);
       if (_path != null && monster != null)
       {
