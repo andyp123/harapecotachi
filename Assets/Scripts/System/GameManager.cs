3321aa00939ca1c157448c747029d7b72eeb7137
Andrew Palmer <andyp.123@gmail.com>
2016-08-02 23:34:52 +0900

    Remove pause function from GameManager

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -4,10 +4,5 @@ using System.Collections;
 // TODO: what really belongs in this class? :)
 public class GameManager : MonoBehaviour
 {
-  // TODO: want a proper pause implementation. This is kind of weird
-  //   also, Game should probably deal with pausing, or have a PauseManager class
-  public void TogglePause ()
-  {
-    Time.timeScale = (Time.timeScale == 0f) ? 1f : 0f;
-  }
+
 }
