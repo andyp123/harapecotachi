bbd467f22450bec1639a194fb297308d7aad5fc6
Andrew Palmer <andyp.123@gmail.com>
2016-08-05 22:17:51 +0900

    Test physical knockback based on damage amount

--- Assets/Scripts/Game/Generic/Damage.cs
+++ Assets/Scripts/Game/Generic/Damage.cs
@@ -6,6 +6,7 @@ public class Damage : MonoBehaviour
   public float _health;
 
   private float _healthMax;
+  private SimplePhysics _physics = null;
 
   public bool Dead
   {
@@ -14,15 +15,30 @@ public class Damage : MonoBehaviour
 
   void Awake ()
   {
+    _physics = gameObject.GetComponent<SimplePhysics>();
     _healthMax = _health;
+  }
+
+  public float ApplyDamage (float damage, Vector3 dir)
+  {
+    float damageTaken = _health;
+    _health = Mathf.Clamp(_health - damage, 0f, _healthMax);
+    damageTaken = damageTaken - _health;
+
+    if (_physics)
+    {
+      _physics.AddForce(dir * damageTaken * Random.Range(1f, 2f));
+    }
+
+    return damageTaken;
   }
 
   public float ApplyDamage (float damage)
   {
-    float oldHealth = _health;
-
+    float damageTaken = _health;
     _health = Mathf.Clamp(_health - damage, 0f, _healthMax);
+    damageTaken = damageTaken - _health;
 
-    return oldHealth - _health;
+    return damageTaken;
   }
 }
