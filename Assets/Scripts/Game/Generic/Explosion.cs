d1a0ee436a92d1e44b3d167541ad76b617f4a42c
Andrew Palmer <andyp.123@gmail.com>
2016-08-06 13:27:31 +0900

    Add explosions and a bomb tower
    Very placeholder still, but working :D

---
+++ Assets/Scripts/Game/Generic/Explosion.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Sensor))]
+public class Explosion : MonoBehaviour
+{
+  public float _damage = 2f;
+  public bool _scaleDamageByDistance = true;
+
+  float _radius = 1f;
+
+  Sensor _sensor;
+
+  void Awake ()
+  {
+    _sensor = gameObject.GetComponent<Sensor>();
+    Collider collider = gameObject.GetComponent<Collider>();
+    _radius = collider.bounds.extents.magnitude; // TODO: this is approximate unless the collider is a sphere
+
+    StartCoroutine(Explode());
+  }
+
+  IEnumerator Explode ()
+  {
+    yield return new WaitForSeconds(0.05f);
+
+    // get nearest n enemies (might want to change the sensor for a sphere cast instead?)
+    TargetInfo[] nearestTargets = _sensor.GetNearestTargets();
+
+    // apply damage to them
+    foreach (TargetInfo t in nearestTargets)
+    {
+      GameObject go = t.target;
+      Damage damage = go.GetComponent<Damage>();
+      if (damage != null)
+      {
+        float damageScale = (_scaleDamageByDistance) ? 1f - (t.sqrDistance / (_radius * _radius)) : 1f;
+        Vector3 dir = (go.transform.position - transform.position).normalized;
+        damage.ApplyDamage(_damage * damageScale, dir);
+      }
+    }
+
+    Destroy(this.gameObject);
+  }
+
+  void OnDrawGizmos ()
+  {
+    Gizmos.color = Color.yellow;
+    Gizmos.DrawWireSphere(transform.position, 5f);
+  }
+}
---
+++ Assets/Scripts/Game/Tower/TowerBomb.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerBomb : Tower
+{
+  public GameObject _towerTop;
+  public GameObject _bombPrefab;
+  public float _rotationSpeed = 60f; // degrees per second
+
+  public float _shotDamage = 1f;
+  public float _shotDelay = 1f; // delay between shots
+  private float _nextAttackEnableTime = 0f;
+
+  private Sensor _sensor = null;
+  private GameObject _target;
+
+  void Awake ()
+  {
+    _sensor = gameObject.GetComponentInChildren<Sensor>();
+  }
+
+  void TrackTarget ()
+  {
+    if (_target != null)
+    {
+      Vector3 targetDir = _target.transform.position - transform.position;
+      targetDir.y = 0;
+      Quaternion targetRotation = Quaternion.LookRotation(targetDir);
+      _towerTop.transform.rotation = Quaternion.RotateTowards(_towerTop.transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+    }
+  }
+
+  void TryAttackTarget ()
+  {
+    if (_target != null && Time.time >= _nextAttackEnableTime)
+    {
+      Vector3 attackPosition = _target.transform.position + new Vector3(Random.value, 0f, Random.value);
+      GameObject.Instantiate(_bombPrefab, attackPosition, Quaternion.identity);
+      _nextAttackEnableTime = Time.time + _shotDelay;
+    }
+  }
+
+  void Update()
+  {
+    if (_target == null)
+    {
+      TargetInfo targetInfo = _sensor.GetNearestTarget();
+      _target = (targetInfo != null) ? targetInfo.target : null;
+    }
+    else
+    {
+      if (_sensor.IsTracked(_target))
+      {
+        TrackTarget();
+        TryAttackTarget();
+      }
+      else
+      {
+        _target = null;
+      }
+    }
+  }
+}
