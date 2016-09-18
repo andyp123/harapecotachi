8a8fee01f883abc2f103e34a1f3e627d1d05369e
AndyP123 <andyp.123@gmail.com>
2016-09-18 15:03:02 +0900

    Add script to destroy objects after time (applied to explosion FX)
    the explosion effect was not being destroyed and ended up clogging up the scene, so I made a simple and generic script that can be used to destroy objects after a set time.

---
+++ Assets/Scripts/Game/Generic/DestroyAfterTime.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+  public float _destroyTime = 1f;
+  public GameObject _target;
+
+  void Start ()
+  {
+    StartCoroutine(DestroyTarget());
+  }
+
+  IEnumerator DestroyTarget ()
+  {
+    yield return new WaitForSeconds(_destroyTime);
+
+    if (_target)
+      Destroy(_target);
+    else
+      Destroy(gameObject);
+  }
+}
