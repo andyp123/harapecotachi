5fc3563feb339cddc40af740e2717425c5119a81
Andrew Palmer <andyp.123@gmail.com>
2017-12-04 19:09:51 +0900

    Fix bug with explosion effect spawning in wrong place
    This was a bug introduced by an update to Unity. Changed the awake function to start and it fixed the problem.

--- Assets/Scripts/Game/Generic/Explosion.cs
+++ Assets/Scripts/Game/Generic/Explosion.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//[RequireComponent(typeof(Sensor))]
 public class Explosion : MonoBehaviour
 {
   public float _radius = 1f;
@@ -10,15 +9,8 @@ public class Explosion : MonoBehaviour
   public bool _spawnOnGround = false;
   public GameObject _explosionPrefab;
 
-  //float _radius = 1f;
-  //Sensor _sensor;
-
-  void Awake ()
+  void Start ()
   {
-    // _sensor = gameObject.GetComponent<Sensor>();
-    // SphereCollider collider = gameObject.GetComponent<SphereCollider>();
-    // _radius = collider.radius; // TODO: will I need other types of collider?
-
     if (_explosionPrefab != null)
     {
       Vector3 pos = transform.position;
@@ -31,13 +23,15 @@ public class Explosion : MonoBehaviour
     }
 
     Explode();
-    // StartCoroutine(Explode());
   }
 
   void Explode ()
   {
+    Vector3 pos = transform.position;
+    if (_spawnOnGround) pos = new Vector3(pos.x, 0f, pos.z);
+
     int layerMask = 1 << LayerMask.NameToLayer("MonsterCollider");
-    Collider[] hitTargets = Physics.OverlapSphere(transform.position, _radius, layerMask, QueryTriggerInteraction.Collide);
+    Collider[] hitTargets = Physics.OverlapSphere(pos, _radius, layerMask, QueryTriggerInteraction.Collide);
 
     // apply damage to them
     foreach (Collider t in hitTargets)
@@ -48,12 +42,12 @@ public class Explosion : MonoBehaviour
       {
         float damageScale = 1f;
         if (_scaleDamageByDistance)
-          damageScale = 1f - ((transform.position - go.transform.position).sqrMagnitude / (_radius * _radius));
+          damageScale = 1f - ((pos - go.transform.position).sqrMagnitude / (_radius * _radius));
 
         DamageInfo info = new DamageInfo();
         info.baseDamage = _damage * damageScale;
         info.knockbackForce = info.baseDamage * Random.Range(1f, 2.5f);
-        info.knockbackDir = (go.transform.position - transform.position).normalized;
+        info.knockbackDir = (go.transform.position - pos).normalized;
         info.damageType = DamageType.Explosive;
 
         damage.ApplyDamage(info);
@@ -63,41 +57,12 @@ public class Explosion : MonoBehaviour
     Destroy(this.gameObject);    
   }
 
-  // IEnumerator Explode ()
-  // {
-  //   // TODO: this allows the sensor time to gather targets
-  //   yield return new WaitForSeconds(0.05f);
-
-  //   // get nearest n enemies
-  //   TargetInfo[] nearestTargets = _sensor.GetNearestTargets();
-
-  //   // apply damage to them
-  //   foreach (TargetInfo t in nearestTargets)
-  //   {
-  //     if (t.target == null) // TODO: the target can be null sometimes, which causes an error. This needs solving
-  //       continue;
-  //     GameObject go = t.target;
-  //     Damage damage = go.GetComponent<Damage>(); 
-  //     if (damage != null)
-  //     {
-  //       float damageScale = (_scaleDamageByDistance) ? 1f - (t.sqrDistance / (_radius * _radius)) : 1f;
-
-  //       DamageInfo info = new DamageInfo();
-  //       info.baseDamage = _damage * damageScale;
-  //       info.knockbackForce = info.baseDamage * Random.Range(2f, 5f);
-  //       info.knockbackDir = (go.transform.position - transform.position).normalized;
-  //       info.damageType = DamageType.Explosive;
-
-  //       damage.ApplyDamage(info);
-  //     }
-  //   }
-
-  //   Destroy(this.gameObject);
-  // }
-
   void OnDrawGizmos ()
   {
+    Vector3 pos = transform.position;
+    if (_spawnOnGround) pos = new Vector3(pos.x, 0f, pos.z);
+
     Gizmos.color = Color.yellow;
-    Gizmos.DrawWireSphere(transform.position, _radius);
+    Gizmos.DrawWireSphere(pos, _radius);
   }
 }
